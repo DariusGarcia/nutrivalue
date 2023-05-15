3a46655914a3325a44996e456e872fe1ff3d6e54
Darius Garcia <dariusgarcia888@gmail.com>
2023-05-15 15:18:50 -0700

    change styling

--- components/layouts/stackedCards.tsx
+++ components/layouts/stackedCards.tsx
@@ -1,12 +1,10 @@
-import { v4 as uuidv4 } from 'uuid'
-
 export default function StackedCardsLayout({ children }: any) {
   return (
     <ul role='list' className='space-y-8 mt-12'>
       {children?.map((item: any) => (
         <li
-          key={uuidv4()}
-          className='overflow-hidden bg-white  py-4 shadow sm:rounded-md '
+          key={item.id}
+          className='overflow-hidden bg-white  py-4 shadow border-t sm:rounded-md '
         >
           {item}
         </li>
--- features/calories/dashboard.tsx
+++ features/calories/dashboard.tsx
@@ -46,7 +46,7 @@ const Dashboard = () => {
           <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:max-w-7xl lg:px-8'>
             <h1 className='sr-only'>Diary dashboard</h1>
             {/* Main 3 column grid */}
-            <div className='grid grid-cols-1 items-start gap-4 lg:grid-cols-3 lg:gap-8'>
+            <div className='grid grid-cols-1 items-start gap-4 lg:grid-cols-1 lg:gap-8'>
               {/* Left column */}
               <div className='grid grid-cols-1 gap-4 lg:col-span-2'>
                 <section aria-labelledby='section-1-title'>
